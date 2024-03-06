c2be85771588da6e309a5e707f2f6a7f2bdfd79b
derwind <uncertainty_principle_ss@yahoo.co.jp>
2024-03-06 19:59:12 +0900

    set valid version

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="ttz", version="mynumpy-mps-0.2", packages=find_packages())
+setup(name="ttz", version="0.2a0.dev0", packages=find_packages())
